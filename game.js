210b08946273b29a1f2cd0b4438e35b13b48e330
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-06-12 11:13:40 -0700

    Make ship object draggable

--- game.js
+++ game.js
@@ -6,12 +6,21 @@ class Ship{
     this.dx = 1;
     this.dy = 0;
     this.dead = false;
+    this.dragX = -1;
+    this.dragY = -1;
   }
 
   draw(){
     ctx.fillStyle = "gray";
     ctx.fillRect(Math.round(this.x)*PIXEL, Math.round(this.y)*PIXEL, this.attributes.width*PIXEL, this.attributes.height*PIXEL);
     
+  }
+
+  checkDrag(){
+    if (mouseInRect(Math.round(this.x)*PIXEL, Math.round(this.y)*PIXEL, this.attributes.width*PIXEL, this.attributes.height*PIXEL)){
+      return true;
+    }
+    return false;
   }
 }
 
@@ -37,14 +46,33 @@ class Game{
   }
 
   render(){
-    ctx.fillStyle = COLOR.TEXT;
-    ctx.textAlign = "left";
-    ctx.font = "40px wendy";
-    ctx.fillText("Fleet Formation",16,32);
-    for (let i=0; i<this.fleet.length; i++){
-      this.fleet[i].draw();
+    if (this.state == 0){
+      ctx.fillStyle = COLOR.TEXT;
+      ctx.textAlign = "left";
+      ctx.font = "40px wendy";
+      ctx.fillText("Fleet Formation",16,32);
+      for (let i=0; i<this.fleet.length; i++){
+        this.fleet[i].draw();
+        if (mouseIsDown && this.heldShip == null){
+          if (this.fleet[i].checkDrag()){
+            this.heldShip = this.fleet[i];
+            this.heldShip.dragX = mouseX/4 - this.heldShip.x;
+            this.heldShip.dragY = mouseY/4 - this.heldShip.y;
+          }
+        }
+      }
+      if (this.heldShip != null){
+        if (mouseIsDown){
+          this.heldShip.x = (mouseX)/4-this.heldShip.dragX;
+          this.heldShip.y = (mouseY)/4-this.heldShip.dragY;
+        }else{
+          this.heldShip = null;
+        }
+      }
+      uiRect(16, 320, 480, 176);
+    }else if (this.state == 1){
+      
     }
-    uiRect(16, 320, 480, 176);
   }
 }
 
