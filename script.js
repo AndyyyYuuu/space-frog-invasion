4cf315d7dabb44755b3a706dfa33f336c07a28cb
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2023-06-11 21:48:47 -0700

    Create game.js
    Create game.js, which contains class Game as a seperate file.

--- script.js
+++ script.js
@@ -94,25 +94,6 @@ class ShootyShip extends Ship{
   }
 }
 
-class Game{
-  constructor(name){
-    this.name = name;
-    this.state = 0;
-    this.fleet = [new ShootyShip(64, 64)];
-    this.heldShip = null;
-  }
-
-  render(){
-    ctx.fillStyle = COLOR.TEXT;
-    ctx.textAlign = "left";
-    ctx.font = "40px wendy";
-    ctx.fillText("Fleet Formation",16,32);
-    for (let i=0; i<this.fleet.length; i++){
-      this.fleet[i].draw();
-    }
-    uiRect(16, 320, 480, 176);
-  }
-}
 
 
 var saveSlots = [
