c0262d47ef9d673b845e7f20f169eef576e7676f
realBarry123 <barryyu28@gmail.com>
2023-12-21 18:34:09 -0800

    Patch saveGames() spelling

--- game.js
+++ game.js
@@ -804,7 +804,7 @@ class Game{
         else if (mouseInRect(36, 84, 56, 8)){ // save and quit
           this.inOptions = false;
           game = null;
-          saveGame();
+          saveGames();
           mode = "start";
         }
       }
